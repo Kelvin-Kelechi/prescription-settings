3ade61b208692ab8c7314c62d1a032d291ba9051
KELEX <ogkelvin99@gmail.com>
2024-12-15 11:00:17 +0100

    fixed

--- src/js/compenents/BreadCrumb/BreadCrumb.tsx
+++ src/js/compenents/BreadCrumb/BreadCrumb.tsx
@@ -1,14 +1,17 @@
-import { MdRefresh } from "react-icons/md";
+import { MdKeyboardArrowLeft, MdRefresh } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const BreadCrumb = () => {
   return (
     <div className="flex sticky top-[72px] w-full items-center px-12 justify-between p-4 border-b bg-white z-10">
       <div className="text-gray-600">
         <span className="text-sm">
+          <MdKeyboardArrowLeft size={25} className="inline-block " />{" "}
+          <MdKeyboardArrowRight size={25} className="inline-block " />{" "}
           <a href="#" className="hover:underline">
             Home
           </a>{" "}
-          &gt;{" "}
+          <MdKeyboardArrowRight size={25} className="inline-block " />{" "}
           <a href="#" className="hover:underline">
             User Profile
           </a>
